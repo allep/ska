a13b03937ad4e830a367a0ec9b82f66e7cf3bda8
Alessandro Paganelli <alessandro.paganelli@gmail.com>
2022-10-31 14:43:09 +0100

    Subdirectories completed

--- src/main/src/main.cpp
+++ src/main/src/main.cpp
@@ -1,10 +1,14 @@
+#include <iostream>
+
 #include "application.h"
 #include "core.h"
 #include "external.h"
-#include <iostream>
+#include "main.h"
+
+const std::string GetMainString() { return "SKA Main"; }
 
 int main(int agrc, char **argv) {
-  std::cout << "Main component linked with:" << std::endl;
+  std::cout << GetMainString() << std::endl;
   std::cout << GetCoreString() << std::endl;
   std::cout << GetApplicationString() << std::endl;
   std::cout << GetExternalString() << std::endl;

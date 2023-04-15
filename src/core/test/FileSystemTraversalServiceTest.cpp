f022213de3199e41408b7857604d2e39ba30ea9b
Alessandro Paganelli <alessandro.paganelli@gmail.com>
2023-04-15 15:34:32 +0200

    WIP: test and interface design

--- src/core/test/FileSystemTraversalServiceTest.cpp
+++ src/core/test/FileSystemTraversalServiceTest.cpp
@@ -3,4 +3,17 @@
 
 #include "FileSystemTraversalServiceMock.h"
 
-TEST(FileSystemTraversalService, BasicTraversal) { EXPECT_EQ(42, 43); }
+TEST(FileSystemTraversalService, BasicTraversal) {
+  // clang-format off
+  EXPECT_EQ(42, 43);
+  // clang-format on
+
+  // Test to do:
+  // create the service mock
+  // set actions for its call as if we were traversing a fs for real
+
+  // TODO FIXME: this probably doesn't work, because I need to specify more than
+  // a single call for a single function call. Is it possible? Is there a better
+  // way to do so? Probably (red hat) I'm doing a bad design of the service
+  // interface :(
+}

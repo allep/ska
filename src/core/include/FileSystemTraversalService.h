0516893e99fa4b9dc5e4d75fdc09194f45fe475d
Alessandro Paganelli <alessandro.paganelli@gmail.com>
2023-04-15 14:58:55 +0200

    WIP: expanded interface and added corresponding mock

--- src/core/include/FileSystemTraversalService.h
+++ src/core/include/FileSystemTraversalService.h
@@ -1,1 +1,22 @@
 #pragma once
+
+#include <filesystem>
+#include <functional>
+
+/**
+ * @brief Interface to a service used to traverse a filesystem subtree
+ * recursively and perform actions on each element.
+ */
+class FileSystemTraversalService {
+public:
+  /**
+   * @brief Recursively traverse a given path performing on each element the
+   * action specified by "action".
+   *
+   * @param path The path to be recursively traversed.
+   * @param action The action to be done on each element.
+   */
+  virtual void RecursivelyVisitDirectory(
+      const std::filesystem::path &path,
+      std::function<void()> action = []() {}) = 0;
+};
